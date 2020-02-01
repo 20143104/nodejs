8c483eaa43b6fcadd464573c87fb6b7cb665ea5c
조승현 <csh4096@gmail.com>
2020-02-01 21:17:15 +0900

    trach file

--- express/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '안녕하세요', data : ['1', 100, '헬로'] });
-});
-
-module.exports = router;
